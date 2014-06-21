9212958165d08b96cd0e30877d6aa402cf2613a7
Matt Didier <mdidier9@gmail.com>
2014-06-20 20:08:17 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Redshirt::Application.config.secret_key_base = '571a2842cf9b09d96a13d97fb5505fb45f64527b20e8e77f1a14dfcbba1f0f19ccc809d4e31d6e7b64bc3b00dcb3581854e9378243d908fa656e9350a44e4040'
